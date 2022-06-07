3265b2e32a9f19edae3685907e2860a9e8eb4d97
sgotarra <95579921+sgotarra@users.noreply.github.com>
2022-06-07 18:45:03 +0200

    original

---
+++ Assets/Scripts/GameObjectConcreteButtonHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+using Pvr_UnitySDKAPI;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class GameObjectConcreteButtonHandler : MonoBehaviour {
+	private Color originalColor;
+	private Color currentColor;
+	
+	private MeshRenderer meshRenderer;
+    
+    private void Awake() {
+		meshRenderer = GetComponent<MeshRenderer> ();
+		originalColor = meshRenderer.material.color;
+	    currentColor = originalColor;
+    }
+
+	private void Update() {
+		
+
+
+	    if (Controller.UPvr_GetKey(0, Pvr_KeyCode.TOUCHPAD))
+	    {
+		    currentColor = Color.cyan;
+		
+	    }
+	    else if (Controller.UPvr_GetKey(0, Pvr_KeyCode.APP))
+	    {
+		    currentColor = Color.red;
+	    }
+	    else if (Controller.UPvr_GetKey(0, Pvr_KeyCode.VOLUMEUP))
+	    {
+		    currentColor = Color.grey;
+	    }
+	    else if (Controller.UPvr_GetKey(0, Pvr_KeyCode.VOLUMEDOWN))
+	    {
+		    currentColor = Color.white;
+	    } 
+	    // Headset buttons: 
+	    else if (Input.GetKey(KeyCode.JoystickButton0))
+	    {
+		    currentColor = Color.black;
+	    }
+	    else
+	    {
+		    currentColor = originalColor;
+	    }
+
+	switch (Controller.UPvr_GetTouchPadClick(0))
+        	{
+            case TouchPadClick.No:
+                break;
+            case TouchPadClick.ClickUp:
+                Debug.LogError("Swipe up");
+				currentColor = Color.red;
+				meshRenderer.material.color = currentColor;
+                break;
+            case TouchPadClick.ClickDown:
+                Debug.LogError("Slide down");
+				currentColor = Color.blue;
+				meshRenderer.material.color = currentColor;
+                
+                break;
+            case TouchPadClick.ClickRight:
+                Debug.LogError("Slide to the right");
+				currentColor = Color.black;
+				meshRenderer.material.color = currentColor;
+                
+                break;
+            case TouchPadClick.ClickLeft:
+                Debug.LogError("Slide to the left");
+				currentColor = Color.yellow;
+				meshRenderer.material.color = currentColor;
+                
+                break;
+            default:
+                break;
+        	}
+		meshRenderer.material.color = currentColor;
+	}
+
+}
+
---
+++ Assets/moupersonatge.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pvr_UnitySDKAPI;
+
+public class moupersonatge : MonoBehaviour {
+
+	private Transform myTransform;
+	private AudioSource source;
+	public AudioClip motor1;
+	public AudioClip motor2;
+	public AudioClip tocabola;
+
+ 	private void OnCollisionEnter(Collision collision)
+
+    {
+       Debug.Log("El " + gameObject.name + " colicionó con el gamobject " + collision.gameObject.name);   
+	   source.Stop();
+	   source.clip =tocabola;
+       source.Play();
+    }
+
+	// Use this for initialization
+	void Start() {
+			myTransform = GetComponent<Transform>();
+			source = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float smooth = 2.0F;
+		Quaternion target;
+		float velocidadTraslac = 2f;
+    	float velocidadRotac = 60f; // X grados por segundo
+		float spped=15f;
+
+		
+		if (transform.position.y < -30f)  
+			{ 
+			
+			Debug.Log ("FIIIN");	
+			Application.Quit();
+			}
+
+
+		if (Input.GetKey("w")) 
+			{
+			myTransform.Translate(new Vector3(0, 0,1) * spped*Time.deltaTime);
+			
+			if (!source.isPlaying)
+        		{
+				source.clip=motor1;
+       			source.Play();
+				}
+			}
+		if (Input.GetKey("s")) myTransform.Translate(new Vector3(0, 0,-1) * spped*Time.deltaTime);
+		if (Input.GetKey("d")) myTransform.Rotate(Vector3.up * velocidadRotac * Time.deltaTime);
+		if (Input.GetKey("a")) myTransform.Rotate(Vector3.down * velocidadRotac * Time.deltaTime);
+
+        
+
+		switch (Controller.UPvr_GetTouchPadClick(0))
+        	{
+            case TouchPadClick.No:
+				if(source.clip!=tocabola) source.Stop();
+                break;
+            case TouchPadClick.ClickUp:
+                myTransform.Translate(new Vector3(0, 0, 1) * spped * Time.deltaTime);
+				if (!source.isPlaying)
+        		{
+				source.clip=motor1;
+       			source.Play();
+				}
+				break;
+            case TouchPadClick.ClickDown:
+                myTransform.Translate(new Vector3(0, 0, -1) * spped* Time.deltaTime);
+				if (!source.isPlaying)
+        		{
+				source.clip=motor1;
+       			source.Play();
+				}
+                break;
+            case TouchPadClick.ClickRight:
+                myTransform.Rotate(Vector3.up * velocidadRotac * Time.deltaTime);
+				if (!source.isPlaying)
+        		{
+				source.clip=motor2;
+       			source.Play();
+				}
+                break;
+            case TouchPadClick.ClickLeft:
+                myTransform.Rotate(Vector3.down * velocidadRotac * Time.deltaTime);
+				if (!source.isPlaying)
+        		{
+				source.clip=motor2;
+       			source.Play();
+				}
+                break;
+            default:
+                break;
+        	}
+	}
+}
